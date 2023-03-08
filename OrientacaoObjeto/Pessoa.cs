6ca042374bba41cebfffb991323e6d9e3ca7fe51
Joo1480 <joabernardelli123@gmail.com>
2023-03-08 10:20:13 -0300

    Criação da classe pessoa, adicionamento dos get e set

--- OrientacaoObjeto/Pessoa.cs
+++ OrientacaoObjeto/Pessoa.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +10,38 @@ namespace OrientacaoObjeto
     internal class Pessoa
     {
         private int Id { get; set; }
-        private string Nome { get; set; }        
+        private string Nome { get; set; }       
+        private string Telefone { get; set;}
+        public Pessoa(int id, string nome, string telefone)
+        {
+            Id = id;
+            Nome = nome;
+            Telefone = telefone;
+        }
+
+        public int getId()
+        {
+            return Id;
+        }
+        public void setId(int id)
+        {
+            Id = id;
+        }
+        public string getNome()
+        {
+            return Nome;
+        }
+        public void setNome(string nome)
+        {
+            Nome = nome;
+        }
+        public string getTelefone()
+        {
+            return Telefone;
+        }
+        public void setTelefone(string telefone)
+        {
+            Telefone = telefone;
+        }
     }
 }
